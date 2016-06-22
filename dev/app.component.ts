45592e4451845eb1db99b82cac1f0bd72a294e1a
Jiwoong Youn <yjw9012job@gmail.com>
2016-06-21 23:07:11 -0700

    2-11: Create my first component

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -1,11 +1,14 @@
 import {Component} from '@angular/core';
+import {MyComponentComponent} from './my-component.component'
 
 @Component({
     selector: 'my-app',
     template: `
         <h1>Angular 2 Boilerplate</h1>
         <p>Hello World!!</p>
+        <my-component></my-component>
     `,
+    directives: [MyComponentComponent]
 })
 export class AppComponent {
 
---
+++ dev/my-component.component.ts
@@ -0,0 +1,15 @@
+/**
+ * Created by yjw9012 on 6/21/16.
+ */
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'my-component',
+    template: `
+        <p>This is my component!</p>
+    `
+})
+
+export class MyComponentComponent {
+
+}
