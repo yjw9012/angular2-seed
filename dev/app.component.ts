86d48ab2b5c37544437c508b3a184c2f368851dc
Jiwoong Youn <yjw9012job@gmail.com>
2016-06-26 18:42:45 -0700

    3-25: Output from custom component

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -7,10 +7,18 @@ import {PropertyBindingComponent} from './property-binding.component';
         <h2>This is the parent component.</h2>
         <h4>Enter your name: </h4>
         <input type="text" [(ngModel)]="name">
-        <my-property [myName]="name" [myAge]="30"></my-property>
+        <my-property
+            [myName]="name"
+            [myAge]="30"
+            (hobbyChanged)="hobby = $event">
+        </my-property>
+
+        <p>My hobby is: {{hobby}}</p>
+
     `,
     directives: [PropertyBindingComponent]
 })
 export class AppComponent {
     name : string = "";
+    hobby : string = "";
 }
--- dev/property-binding.component.ts
+++ dev/property-binding.component.ts
@@ -1,20 +1,28 @@
 /**
  * Created by yjw9012 on 6/26/16.
  */
-import {Component, Input} from "@angular/core";
+import {Component, Input, EventEmitter} from "@angular/core";
 
 @Component({
     selector: "my-property",
     template: `
         <h3>This is the child component.</h3>
-
         <p>Hi {{name}}! I am {{age}} years old.</p>
+        <h4>My hobby is: </h4>
+        <input type="text" #inputForHobby (keyup)="onHobbyChanged(inputForHobby.value)">
     `,
-    inputs: ['name:myName']
+    inputs: ['name:myName'],
+    outputs: ['hobbyChanged']
 })
 
 export class PropertyBindingComponent {
     name : string = "Jiwoong";
     @Input("myAge") age : number = 25;
+
+    hobbyChanged = new EventEmitter<string>();
+
+    onHobbyChanged(hobby : string) : void {
+        this.hobbyChanged.emit(hobby);
+    }
 
 }
